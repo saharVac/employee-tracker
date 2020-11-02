02ffd1d8701a00495322ac9eab18503645077e4f
Sahar Vacnich <saharvacnich@Sahars-MacBook-Pro.local>
2020-11-02 15:11:30 -0500

    Enabled user to access and manipulate info until choosing to exit

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 // Dependencies
 const mysql = require("mysql")
 const inquirer = require("inquirer")
+
+async function userAction() {
+    const {action} = await inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['Add', 'View', 'Update', 'Exit']
+    })
+    return action
+}
+
+
+async function init() {
+    // keep it going until user wants to exit
+    let exit = false
+    while (!exit) {
+        const act = await userAction()  
+        if (act !== 'Exit') {
+
+        } else {
+            exit = true;
+        }
+    }
+}
+
+init()
